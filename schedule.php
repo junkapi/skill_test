b3acfeb1e7cfb21961a6b65ff1066071f25dab4f
junkapi <numagamijn@gmail.com>
2018-07-16 08:05:44 +0800

    コンテンツずれ修正

--- schedule.php
+++ schedule.php
@@ -71,32 +71,22 @@
 
         <h2 class="text-center content_header" style="font-family: 'Hannotate SC',sans-serif;">タスク管理</h2>
 
-        <div class="col-xs-4">
-          <a href="post.php" class="btn btn-primary button" style="font-family: 'Hannotate SC',sans-serif;">追加</a>
+        <div class="col-xs-4"><a href="post.php" class="btn btn-primary button" style="font-family: 'Hannotate SC',sans-serif;">追加</a></div>
+        <?php  foreach ($comments as $comment): ?>
 
-        </div>
-
-       <?php  foreach ($comments as $comment): ?>
-
-        <div class="col-xs-8">
+         <div class="col-xs-8">
           <div class="task">
            <h2 style="font-family: 'Hannotate SC',sans-serif;"><span><?php echo $comment['date'] ?></span></h2>
             <div class="box14">
-             <h3 style="font-family: 'Hannotate SC',sans-serif;"> <a href="detail.php" style="font-weight: bold;"><?php echo $comment['title'] ?></a>
+             <h3 style="font-family: 'Hannotate SC',sans-serif;"> 
+              <a href="detail.php" style="font-weight: bold;"><?php echo $comment['title'] ?></a>
               <a href="edit.php?id=<?php echo $comment["id"]; ?>" class="btn btn-success" style="color: white">編集</a>
-              <a href="delete.php?id=<?php echo $comment["id"]; ?>" class="btn btn-danger" style="color: white">削除</a>
-
-             </h3>
+              <a href="delete.php?id=<?php echo $comment["id"]; ?>" class="btn btn-danger" style="color: white">削除</a></h3>
               <p style="font-family: 'Hannotate SC',sans-serif;"><?php echo $comment['detail'] ?></p>
             </div>
           </div>
-
-       <?php endforeach; ?>
-
-
-            </div>
-          </div>
         </div>
+        <?php endforeach; ?>
 
       </div>
     </div>
